90d4ab50704d343330837e6dcfcef35043d89ce7
Sean Kelly <spkellydev@gmail.com>
2018-01-19 23:02:51 -0500

    omdb/cmd

--- liri.js
+++ liri.js
@@ -2,8 +2,9 @@ const { setTimeout } = require('timers')
 const keys = require('./keys')
 const twitter = require('./lib/twitter')
 const spotify = require('./lib/spotify')
-const request = require('request')
 const inquirer = require('inquirer')
+const omdb = require('./lib/omdb')
+const { cmd } = require('./lib/cmd')
 let {command, subprocess} = require('./argv')
 
 
@@ -32,8 +33,22 @@ switch (command) {
         spotify.spotifyThis(subprocess)
         break
     case 'movie-this':
-        console.log(subprocess)
+        omdb.omdbMovieThis(subprocess)
         break
+    case 'do-what-it-says':
+        cmd()
+        break
+    case '--help':
+        console.log(`
+        Your Choices are to run "node liri.js" followed by:
+            my-tweets                           ~|>>>> This will return the last 20 tweets from NodeJS
+            spotify-this-song <song name here>  ~|>>>> This will return song data from Spotify
+            movie-this <movie title here>       ~|>>>> This will return movie data from OMDbAPI
+            do-what-it-says                     ~|>>>> This will return something random
+        `)
+        break
+    default:
+        console.log(`RUN   node liri.js --help`)
 
  
 
