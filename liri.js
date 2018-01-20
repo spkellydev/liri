025df2380e277c21731c900b81c72e01cba4f8b2
Sean Kelly <spkellydev@gmail.com>
2018-01-19 20:41:21 -0500

    twitter inquirer

--- liri.js
+++ liri.js
@@ -1,5 +1,36 @@
+const { setTimeout } = require('timers')
+
 const keys = require('./keys')
 const twitter = require('./twitter/twitter')
 const request = require('request')
+const inquirer = require('inquirer')
 const fs = require('fs')
+
+let command = process.argv[2]
+switch (command) {
+    case 'my-tweets':
+        twitter.getTweets()
+        let tweets = twitter.readTweets()
+        if (tweets.length === 0) {
+            setTimeout(function () {
+
+                var questions = [{
+                    name: 'tweets',
+                    message: 'Last 20 Tweets',
+                    type: 'list',
+                    choices: tweets,
+                    pageSize: 30
+                }]
+        
+                inquirer.prompt(questions, function (answer) {
+                    console.log(answer)
+                })
+            }, 700)
+        }
+ 
+
+        
+}
+
+// twitter.getTweets()
 
--- twitter/twitter.js
+++ twitter/twitter.js
@@ -2,15 +2,30 @@ const twitter = require('twitter')
 const keys = require('../keys')
 const fs = require('fs')
 let client = new twitter(keys.twitter)
+let data = []
 
-var params = {screen_name: 'nodejs'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    tweets = JSON.stringify(tweets)
-    fs.writeFile('./tweetData.json', tweets, (err) => {
-        if (err) console.log(err)
-    })
-  } else {
-    console.log(error)
-  }
-});
+function getTweets () {
+    let params = {screen_name: 'nodejs', count: 20};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+    if (!error) {
+        // console.log(response)
+        tweets = JSON.parse(response.body)
+        tweets.forEach(tweet => {
+            data.push(tweet.text)
+        })
+        return data
+
+    } else {
+        console.log(error)
+    }
+    });
+}
+
+function readTweets() {
+    return data
+}
+
+module.exports = {
+    getTweets,
+    readTweets
+}
